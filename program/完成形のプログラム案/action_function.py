b2a5f40ae2fe72ac5ce675880b43a414e1fa4db9
mountain_a <nisshi5114@gmail.com>
2026-03-01 22:30:38 +0900

    updated action func

--- program/完成形のプログラム案/action_function.py
+++ program/完成形のプログラム案/action_function.py
@@ -1,2 +1,24 @@
+
+def random_action():
+    return 0
+
+def breakfunc():
+    return 1
+
+
+
+actions={
+    1: random_action,
+    2: breakfunc,
+
+}
+
+
+
+
+
+
 def action(action_number):
+    target_func = actions.get(action_number)
+    target_func()
     return action_number
